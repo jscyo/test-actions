e6548c820b26572f03fe21ed5f4472dcdea45d44
jscyo <joelsteve97@hotmail.com>
2022-03-30 11:45:55 +0530

    action now downloads and outputs go version

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+echo "Download the go binary"
+
+wget https://golang.org/dl/go1.17.2.linux-amd64.tar.gz
+
+echo "Unzip the binary and move the archive to /usr/local"
+tar -C /usr/local -xzf go1.17.2.linux-amd64.tar.gz
+
+echo "Add the go bin path to the terminal environment."
+export PATH=$PATH:/usr/local/go/bin
+
+version=$go version
+echo "::go version::$version"
